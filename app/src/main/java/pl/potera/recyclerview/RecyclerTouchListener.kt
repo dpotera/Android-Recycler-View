0493a4088f6aeeb014885f6b805a43b33d4d3840
dpotera <dominik.potera@gmail.com>
2017-05-14 05:00:23 +0200

    Moved to kotlin; Added movie images; Added Movie Activity;

---
+++ app/src/main/java/pl/potera/recyclerview/RecyclerTouchListener.kt
@@ -0,0 +1,40 @@
+package pl.potera.recyclerview
+
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.GestureDetector
+import android.view.MotionEvent
+
+class RecyclerTouchListener(context: Context, recyclerView: RecyclerView,
+        private val clickListener: MainActivity.ClickListener):RecyclerView.OnItemTouchListener {
+
+    private val gestureDetector: GestureDetector
+
+    init {
+        gestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent) = true
+
+            override fun onLongPress(e: MotionEvent) {
+                val child = recyclerView.findChildViewUnder(e.x, e.y)
+                if (child != null) {
+                    clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child))
+                }
+            }
+        })
+    }
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        val child = rv.findChildViewUnder(e.x, e.y)
+        if (child != null && gestureDetector.onTouchEvent(e)) {
+            clickListener.onClick(child, rv.getChildAdapterPosition(child))
+        }
+        return false
+    }
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+
+    }
+}
